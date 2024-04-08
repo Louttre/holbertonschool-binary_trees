53a6224e4566bb414dd541625986deef63a393be
SamJouini <samantha.u.h.jouini@gmail.com>
2024-04-08 20:13:04 +0200

    fixing a mistake

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,23 +8,28 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height = 0;
-	size_t right_height = 0;
+	size_t leftHeight = 0;
+	size_t rightHeight = 0;
 
 	if (tree == NULL)
 	{
-		return (-1);
+		return (0);
 	}
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
-
-	if (left_height > right_height)
+	if (tree->right == NULL && tree->left == NULL)
 	{
-		return (left_height + 1);
+		return (0);
+	}
+
+	leftHeight = binary_tree_height(tree->left);
+	rightHeight = binary_tree_height(tree->right);
+
+	if (leftHeight > rightHeight)
+	{
+		return (leftHeight + 1);
 	}
 	else
 	{
-		return (right_height + 1);
+		return (rightHeight + 1);
 	}
 }
