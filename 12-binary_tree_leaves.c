11b3e917dd4c2242344bf9be577a5ff7b2f2efbe
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-04-08 17:45:56 +0200

    Update 12-binary_tree_leaves.c

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -13,10 +13,7 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 		return (0);
 	}
 	if (!tree->right && !tree->left)
-	{
-		count++;
-		return (count);
-	}
+		return (count + 1);
 	count += binary_tree_leaves(tree->left);
 	count += binary_tree_leaves(tree->right);
 	return (count);
