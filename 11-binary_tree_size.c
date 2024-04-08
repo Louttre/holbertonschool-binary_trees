11afafa461961dbe586fac1be6aa4f5fb9153214
root <seth.rueda@gmail.com>
2024-04-08 15:08:59 +0000

    add 11-binary_tree_size.c

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t count = 0;
+
+	if (!tree)
+		return (0);
+	count += binary_tree_size(tree->left);
+	count += binary_tree_size(tree->right);
+	return (count + 1);
+}
