2db1d1bced9658bd6460bc05efaf5136b2ee2100
SamJouini <samantha.u.h.jouini@gmail.com>
2024-04-08 13:56:22 +0200

    removing extras

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -32,7 +32,6 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	else
 	{
 		newNode->left = parent->left;
-		newNode->right = NULL;
 		parent->left->parent = newNode;
 		parent->left = newNode;
 	}
