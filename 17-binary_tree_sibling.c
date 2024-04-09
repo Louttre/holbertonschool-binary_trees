b7466150cb8b75b85bba5add8717e6892e38f55d
root <seth.rueda@gmail.com>
2024-04-09 10:37:43 +0000

    add 17-binary_tree_sibling.c

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+/**
+ *
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	binary_tree_t *temp;
+
+	if (!node || !node->parent)
+		return (NULL);
+	temp = node->parent;
+	if(temp->right->n == node->n && temp->left)
+		return (temp->left);
+	else if (temp->right)
+		return (temp->right);
+	return (NULL);
+}
