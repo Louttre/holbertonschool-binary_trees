fa62aea932e42b439bb08654c53607adc85c0d44
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-04-09 15:40:16 +0200

    Update 18-binary_tree_uncle.c

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -14,7 +14,7 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 	temp = node->parent->parent;
 	if (temp->right->n == node->parent->n  && temp->left)
 		return (temp->left);
-	else if (temp->right)
+	if (temp->left->n == node->parent->n  && temp->right)
 		return (temp->right);
 	return (NULL);
 }
