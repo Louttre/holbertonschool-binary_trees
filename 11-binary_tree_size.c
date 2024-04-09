337734beed1b31ba7e318794fe2944709590f679
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-04-09 14:35:57 +0200

    Update 11-binary_tree_size.c

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -8,11 +8,10 @@
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	size_t count = 0;
-
-	if (!tree)
+	if (tree == NULL)
+	{
 		return (0);
-	count += binary_tree_size(tree->left);
-	count += binary_tree_size(tree->right);
-	return (count + 1);
+	}
+	
+	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
 }
