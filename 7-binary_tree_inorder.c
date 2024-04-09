9042b22ab1a37855528fc607a67f6ca16fab8607
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-04-09 13:27:32 +0200

    Update 7-binary_tree_inorder.c

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 #include <stdlib.h>
 /**
- * binary_tree_inorder - function that goes through a binary tree using inorder traversal
+ * binary_tree_inorder - goes through a binary tree using inorder traversal
  * @tree: pointer to the root of the tree
  * @func: function pointer
  */
