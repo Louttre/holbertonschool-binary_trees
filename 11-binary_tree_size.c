7ab6dad7c6fd82d77edfc8e25e9ff00a7d4a0090
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-04-09 12:58:19 +0200

    Update 11-binary_tree_size.c

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -2,7 +2,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- *
+ * binary_tree_size - function that measures the size of a binary tree
+ * @tree: tree to measure
+ * Return: number of node in the tree
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
