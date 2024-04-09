c1010acbaf7fe51db6029163e3894bbbb288f74d
SamJouini <samantha.u.h.jouini@gmail.com>
2024-04-09 15:01:55 +0200

    {}

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -9,7 +9,6 @@
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-{
 	binary_tree_t *newNode;
 
 	if (parent == NULL)
@@ -38,5 +37,4 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	}
 
 	return (newNode);
-}
 }
