fd7e9bef4c1e14c5afe7ea7b05b44f3f966faa2f
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-04-09 14:52:30 +0200

    Update 12-binary_tree_leaves.c

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -6,7 +6,7 @@
  * @tree: tree to count the leaves
  * Return: number of leaves in the tree
  */
-size_t binary_tree_leaves(const binary_tree_ttree)
+size_t binary_tree_leaves(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 	{
