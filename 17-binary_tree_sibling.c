76d70c3cd7f5f84e6ab1c6f060c4327dfc90d22b
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-04-09 13:24:20 +0200

    Update 17-binary_tree_sibling.c

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,7 +1,9 @@
 #include "binary_trees.h"
 #include <stdlib.h>
 /**
- *
+ * binary_tree_sibling - checks if a binary tree is perfect
+ * @node: pointer to the node
+ * Return: pointer to the sibling or NULL
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
